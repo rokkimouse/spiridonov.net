c296d07eb0976b34b92c157cd2551dbbe0c7e148
Dmitriy Spiridonov <dmit.spir@yandex.ru>
2020-10-26 16:48:11 +0300

    add nemiko scrpt

---
+++ python/networking/sandbox/netmiko_get.py
@@ -0,0 +1,16 @@
+from netmiko import ConnectHandler
+
+
+access = {"host": "ios-xe-mgmt.cisco.com", "port": "8181", 
+          "username": "developer", "password": "C1sco12345", "device_type": "cisco_ios"}
+
+
+try:
+    connection = ConnectHandler(**access)
+    connection.enable()
+    sh_run = connection.send_command("show run")
+    connection.disconnect()
+except Exception as ex:
+    print(ex)
+else:
+    print(sh_run)
