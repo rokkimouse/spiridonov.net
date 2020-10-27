238b1d4ec07cb385c88433e95c78ffcd4019f729
Dmitriy Spiridonov <dmit.spir@yandex.ru>
2020-10-27 15:02:21 +0300

    del some ex

--- python/networking/sandbox/netmiko_get.py
+++ python/networking/sandbox/netmiko_get.py
@@ -2,7 +2,8 @@ from netmiko import ConnectHandler
 
 
 access = {"host": "ios-xe-mgmt.cisco.com", "port": "8181", 
-          "username": "developer", "password": "C1sco12345", "device_type": "cisco_ios"}
+          "username": "developer", "password": "C1sco12345", 
+          "device_type": "cisco_ios"}
 
 
 try:
