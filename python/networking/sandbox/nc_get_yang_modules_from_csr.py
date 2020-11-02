3e90909d517fa13a5e6d39d7278af6bbafab2607
Dmitriy Spiridonov <dmit.spir@yandex.ru>
2020-11-02 16:07:26 +0300

    edit creds

--- python/networking/sandbox/nc_get_yang_modules_from_csr.py
+++ python/networking/sandbox/nc_get_yang_modules_from_csr.py
@@ -1,12 +1,16 @@
 from ncclient import manager
 
 
-access = {"host": "ios-xe-mgmt.cisco.com", "port": "10000", 
-          "username": "developer", "password": "C1sco12345"}
+access = {
+    "host": "ios-xe-mgmt.cisco.com", 
+    "port": "10000", 
+    "username": "developer", 
+    "password": "C1sco12345",
+    "hostkey_verify" : False 
+    }
 
 
-with manager.connect(host=access["host"], port=access["port"], username=access["username"], 
-                     password=access["password"], hostkey_verify=False) as conection:
+with manager.connect(**access) as conection:
     for module in conection.server_capabilities:
         print(module)
     conection.close_conections
