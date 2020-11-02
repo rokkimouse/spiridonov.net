e2d6d5756795b49d4fce782106c24ce848d3dc2b
Dmitriy Spiridonov <dmit.spir@yandex.ru>
2020-11-02 18:44:34 +0300

    add script

--- python/networking/sandbox/netmiko_get_run_conf.py
+++ python/networking/sandbox/netmiko_get_run_conf.py
@@ -1,9 +1,13 @@
 from netmiko import ConnectHandler
 
 
-access = {"host": "ios-xe-mgmt.cisco.com", "port": "8181", 
-          "username": "developer", "password": "C1sco12345", 
-          "device_type": "cisco_ios"}
+access = {
+    "host": "ios-xe-mgmt.cisco.com", 
+    "port": "8181", 
+    "username": "developer", 
+    "password": "C1sco12345", 
+    "device_type": "cisco_ios"
+    }
 
 
 try:
